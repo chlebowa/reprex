7768a79d53cfec2d3e733e150e5364cc72ad07ad
Aleksander Chlebowski <aleksander.chlebowski@sanofi.com>
2025-04-09 21:24:43 +0200

    add app file

---
+++ app.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+ui <- fluidPage(
+		  column(3, textInput("text", "some text"), textOutput("print"))
+		  )
+
+server <- function(input, output, session) {
+	  text <- reactive( input$text )
+  output[["print"]] <- renderText( req(text()) )
+}
+
+shinyApp(ui, server)
+
